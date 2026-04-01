ca4a47f7fbbd07bb5ab827734ecd91b85d37ca3f
Oleksandr Grinenko <69903565+Oleksandr1072004@users.noreply.github.com>
2026-04-01 09:29:55 +0300

    Update sensors.js

--- autoscan-pro_project/routes/sensors.js
+++ autoscan-pro_project/routes/sensors.js
@@ -1,1 +1,10 @@
+const router = require('express').Router();
+const { verifyToken } = require('../middleware/auth');
+const sensorController = require('../controllers/sensorController');
 
+router.use(verifyToken);
+
+router.get('/live',              sensorController.getLive);
+router.get('/history/:sessionId',sensorController.getHistory);
+
+module.exports = router;
