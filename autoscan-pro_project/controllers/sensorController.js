fa7bd01d4b31483b3cad792e5473f68917f246e4
Oleksandr Grinenko <69903565+Oleksandr1072004@users.noreply.github.com>
2026-04-01 09:28:14 +0300

    Update sensorController.js

--- autoscan-pro_project/controllers/sensorController.js
+++ autoscan-pro_project/controllers/sensorController.js
@@ -1,1 +1,33 @@
+const db = require('../config/db');
+const sensorService = require('../services/sensorService');
 
+/**
+ * GET /api/v1/sensors/live
+ * Returns current snapshot of all live sensor values from OBD-II
+ */
+const getLive = async (req, res, next) => {
+  try {
+    const data = await sensorService.getLiveSnapshot();
+    res.json(data);
+  } catch (err) {
+    next(err);
+  }
+};
+
+/**
+ * GET /api/v1/sensors/history/:sessionId
+ * Returns all sensor readings recorded during a scan session
+ */
+const getHistory = async (req, res, next) => {
+  try {
+    const readings = await db('sensor_readings')
+      .where({ session_id: req.params.sessionId })
+      .orderBy('recorded_at', 'asc');
+
+    res.json(readings);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { getLive, getHistory };
