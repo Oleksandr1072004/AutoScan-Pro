84512202deb60816bad359b70556bdff19e616ac
Oleksandr Grinenko <69903565+Oleksandr1072004@users.noreply.github.com>
2026-04-01 09:29:33 +0300

    Update vehicles.js

--- autoscan-pro_project/routes/vehicles.js
+++ autoscan-pro_project/routes/vehicles.js
@@ -1,1 +1,13 @@
+const router = require('express').Router();
+const { verifyToken } = require('../middleware/auth');
+const vehicleController = require('../controllers/vehicleController');
 
+router.use(verifyToken);
+
+router.get('/',     vehicleController.getAll);
+router.post('/',    vehicleController.create);
+router.get('/:id',  vehicleController.getById);
+router.patch('/:id',vehicleController.update);
+router.delete('/:id',vehicleController.remove);
+
+module.exports = router;
