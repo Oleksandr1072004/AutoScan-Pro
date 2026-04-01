2737d0b49edc3e3d1c966d42e4307b5e1e554e2c
Oleksandr Grinenko <69903565+Oleksandr1072004@users.noreply.github.com>
2026-04-01 09:30:18 +0300

    Update scans.js

--- autoscan-pro_project/routes/scans.js
+++ autoscan-pro_project/routes/scans.js
@@ -1,1 +1,13 @@
+const router = require('express').Router();
+const { verifyToken } = require('../middleware/auth');
+const scanController = require('../controllers/scanController');
 
+router.use(verifyToken);
+
+router.post('/',                           scanController.initiateScan);
+router.get('/:id',                         scanController.getResult);
+router.get('/history/:vehicleId',          scanController.getHistory);
+router.delete('/:vehicleId/clear-dtc',     scanController.clearDtc);
+router.delete('/:id',                      scanController.deleteSession);
+
+module.exports = router;
