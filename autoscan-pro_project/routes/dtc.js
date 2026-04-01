7726effe5e605d7ece77e7cf6d58bc1cc0fdcc87
Oleksandr Grinenko <69903565+Oleksandr1072004@users.noreply.github.com>
2026-04-01 09:30:35 +0300

    Update dtc.js

--- autoscan-pro_project/routes/dtc.js
+++ autoscan-pro_project/routes/dtc.js
@@ -1,1 +1,9 @@
+const router = require('express').Router();
+const { verifyToken } = require('../middleware/auth');
+const dtcController = require('../controllers/dtcController');
 
+router.use(verifyToken);
+
+router.get('/:code', dtcController.getByCode);
+
+module.exports = router;
